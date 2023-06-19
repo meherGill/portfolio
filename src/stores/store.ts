e284c049e0898157316fed213d2f997f0eb9d980
meher <meher310van@gmail.com>
2023-06-20 02:07:50 +1000

    screwed up styling, save point

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -5,3 +5,6 @@ export const $isIntroDone = atom(initialValueForIsDone)
 
 const initialThemeValue = "night"
 export const $theme = atom<"night" | "garden">(initialThemeValue)
+
+const sphereTranslateMapper = {'a': 1}
+export const $sphereTranslateProps = atom<{}>(sphereTranslateMapper)
