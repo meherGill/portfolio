84a87a487713c57d90145140239bb1da3231a6aa
meher <meher310van@gmail.com>
2023-06-27 18:05:03 +1000

    sync flip cards color with bg on contactpage

--- src/components/Card/FlipCard/FlipCard.tsx
+++ src/components/Card/FlipCard/FlipCard.tsx
@@ -59,7 +59,7 @@ export default ({
             <div
                 ref={cardRefContent}
                 id={passedId}
-                className="card__content rounded-md bg-slate-900"
+                className="card__content rounded-md bg-[#151515]"
             >
                 <div className="front__card absolute top-0 bottom-0 left-0 right-0 flex flex-col items-stretch justify-center">
                     <div
